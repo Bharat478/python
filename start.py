3156998dc70111019401e63388a5b18e80b46fef
Bharat478 <159224960+Bharat478@users.noreply.github.com>
2025-04-05 12:07:30 +0545

    firstpush

---
+++ start.py
@@ -0,0 +1,17 @@
+# learning list and tuples
+
+
+friends = ["apple", "orange", 0 , 36.5 , "aakash"]
+print(friends[0])
+print(friends[1:3])
+friends.append("Bharat")
+print(friends)
+
+
+l1 = [1, 2, 3, 4, 5, 6]
+l1.sort()
+print(l1)
+# l1.reverse() reverse gardinxa value list ma vako value lai
+print(l1)
+l1.insert(3,2.5)
+print(l1)
